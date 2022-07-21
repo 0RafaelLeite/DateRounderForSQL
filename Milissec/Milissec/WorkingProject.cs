cb4d9ae0d1341f87542fe22e3d96fb98e9b563ee
RafaelRocha22 <rafaelrocha.git@outlook.com>
2022-07-21 16:55:42 -0300

    Lógica funcionando

---
+++ Milissec/Milissec/WorkingProject.cs
@@ -0,0 +1,56 @@
+﻿DateTime DataAtual = new DateTime(2022, 12, 31, 23, 59, 59, 999);
+
+
+var MsAtual = DataAtual.Millisecond;
+int cent = MsAtual / 100;
+int dez = (MsAtual - cent * 100) / 10;
+int uni = (MsAtual - cent * 100) - dez * 10;
+DateTime DataArredondada;
+
+
+Console.WriteLine("Data inicial = " + DataAtual);
+Console.WriteLine(DataAtual.Millisecond);
+
+switch (uni)
+{
+    case 1:
+        DataArredondada = DataAtual.AddMilliseconds(-1);
+        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
+        DataAtual = DataArredondada;
+        break;
+    case 2:
+        DataArredondada = DataAtual.AddMilliseconds(1);
+        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
+        DataAtual = DataArredondada;
+        break;
+    case 4:
+        DataArredondada = DataAtual.AddMilliseconds(-1);
+        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
+        DataAtual = DataArredondada;
+        break;
+    case 5:
+        DataArredondada = DataAtual.AddMilliseconds(2);
+        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
+        DataAtual = DataArredondada;
+        break;
+    case 6:
+        DataArredondada = DataAtual.AddMilliseconds(1);
+        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
+        DataAtual = DataArredondada;
+        break;
+    case 8:
+        DataArredondada = DataAtual.AddMilliseconds(-1);
+        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
+        DataAtual = DataArredondada;
+        break;
+    case 9:
+        DataArredondada = DataAtual.AddMilliseconds(1);
+        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
+        DataAtual = DataArredondada;
+        break;
+    default:
+        break;
+}
+
+Console.WriteLine("Data final = " + DataAtual);
+Console.WriteLine(DataAtual.Millisecond);
