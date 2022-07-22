4a31ba8ab7e3ea858245d520efd6dc568809b8b7
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2022-07-22 01:00:57 -0300

    otimização e criacao classe

---
+++ Milissec/Milissec/DateConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milissec
+{
+    public class DateConverter
+    {
+        public DateTime DataAtual { get; set; }
+
+        public DateConverter(DateTime dataAtual)
+        {
+            this.DataAtual = dataAtual;
+        }
+
+        public void ArredondaData()
+        {
+            var MsAtual = DataAtual.Millisecond;
+            int cent = MsAtual / 100;
+            int dez = (MsAtual - cent * 100) / 10;
+            int uni = (MsAtual - cent * 100) - dez * 10;
+            DateTime DataArredondada;
+
+            switch (uni)
+            {
+                case 1:
+                    DataArredondada = DataAtual.AddMilliseconds(-1);
+                    DataAtual = DataArredondada;
+                    break;
+                case 2:
+                    DataArredondada = DataAtual.AddMilliseconds(1);
+                    DataAtual = DataArredondada;
+                    break;
+                case 4:
+                    DataArredondada = DataAtual.AddMilliseconds(-1);
+                    DataAtual = DataArredondada;
+                    break;
+                case 5:
+                    DataArredondada = DataAtual.AddMilliseconds(2);
+                    DataAtual = DataArredondada;
+                    break;
+                case 6:
+                    DataArredondada = DataAtual.AddMilliseconds(1);
+                    DataAtual = DataArredondada;
+                    break;
+                case 8:
+                    DataArredondada = DataAtual.AddMilliseconds(-1);
+                    DataAtual = DataArredondada;
+                    break;
+                case 9:
+                    DataArredondada = DataAtual.AddMilliseconds(1);
+                    DataAtual = DataArredondada;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Milissec/Milissec/WorkingProject.cs
+++ Milissec/Milissec/WorkingProject.cs
@@ -1,56 +1,49 @@
-﻿DateTime DataAtual = new DateTime(2022, 12, 31, 23, 59, 59, 999);
+﻿using System;
+using Milissec;
 
-
-var MsAtual = DataAtual.Millisecond;
-int cent = MsAtual / 100;
-int dez = (MsAtual - cent * 100) / 10;
-int uni = (MsAtual - cent * 100) - dez * 10;
-DateTime DataArredondada;
-
-
-Console.WriteLine("Data inicial = " + DataAtual);
-Console.WriteLine(DataAtual.Millisecond);
-
-switch (uni)
+class WorkingProject
 {
-    case 1:
-        DataArredondada = DataAtual.AddMilliseconds(-1);
-        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
-        DataAtual = DataArredondada;
-        break;
-    case 2:
-        DataArredondada = DataAtual.AddMilliseconds(1);
-        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
-        DataAtual = DataArredondada;
-        break;
-    case 4:
-        DataArredondada = DataAtual.AddMilliseconds(-1);
-        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
-        DataAtual = DataArredondada;
-        break;
-    case 5:
-        DataArredondada = DataAtual.AddMilliseconds(2);
-        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
-        DataAtual = DataArredondada;
-        break;
-    case 6:
-        DataArredondada = DataAtual.AddMilliseconds(1);
-        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
-        DataAtual = DataArredondada;
-        break;
-    case 8:
-        DataArredondada = DataAtual.AddMilliseconds(-1);
-        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
-        DataAtual = DataArredondada;
-        break;
-    case 9:
-        DataArredondada = DataAtual.AddMilliseconds(1);
-        Console.WriteLine("Data Arredondada = " + DataArredondada.Millisecond);
-        DataAtual = DataArredondada;
-        break;
-    default:
-        break;
-}
+    static void Main(string[] args)
+    {
+        Console.WriteLine("----------------Primeira Data------------------------");
+        var Data1 = new DateConverter(new DateTime(2022, 12, 31, 23, 59, 59, 997));
+        Console.WriteLine("Data obtida: " + Data1.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data1.DataAtual.Millisecond);
+        Data1.ArredondaData();
+        Console.WriteLine("Data Arredondada: " + Data1.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data1.DataAtual.Millisecond);
 
-Console.WriteLine("Data final = " + DataAtual);
-Console.WriteLine(DataAtual.Millisecond);
+        Console.WriteLine("----------------Segunda Data------------------------");
+        var Data2 = new DateConverter(new DateTime(2022, 12, 31, 23, 59, 59, 542));
+        Console.WriteLine("Data obtida: " + Data2.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data2.DataAtual.Millisecond);
+        Data2.ArredondaData();
+        Console.WriteLine("Data Arredondada: " + Data2.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data2.DataAtual.Millisecond);
+
+        Console.WriteLine("----------------Terceira Data------------------------");
+        var Data3 = new DateConverter(new DateTime(2022, 12, 31, 23, 24, 59, 998));
+        Console.WriteLine("Data obtida: " + Data3.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data3.DataAtual.Millisecond);
+        Data3.ArredondaData();
+        Console.WriteLine("Data Arredondada: " + Data3.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data3.DataAtual.Millisecond);
+
+        Console.WriteLine("----------------Quarta Data------------------------");
+        var Data4 = new DateConverter(new DateTime(2022, 12, 31, 23, 59, 59, 991));
+        Console.WriteLine("Data obtida: " + Data4.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data4.DataAtual.Millisecond);
+        Data4.ArredondaData();
+        Console.WriteLine("Data Arredondada: " + Data4.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data4.DataAtual.Millisecond);
+
+        Console.WriteLine("----------------Quinta Data------------------------");
+        var Data5 = new DateConverter(new DateTime(2022, 12, 31, 23, 59, 59, 999));
+        Console.WriteLine("Data obtida: " + Data5.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data5.DataAtual.Millisecond);
+        Data5.ArredondaData();
+        Console.WriteLine("Data Arredondada: " + Data5.DataAtual);
+        Console.WriteLine("Milissegundos: " + Data5.DataAtual.Millisecond);
+
+    }
+}
